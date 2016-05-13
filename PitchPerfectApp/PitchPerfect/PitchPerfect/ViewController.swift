4b65d4451b897b0fd49c1cfd917aadafd9be669f
Yang Zhou <zhouyang6828629@gmail.com>
2016-05-13 09:29:06 -0700

    A new game pirate fleet

--- PitchPerfectApp/PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfectApp/PitchPerfect/PitchPerfect/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RecordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(sender: AnyObject) {
+        print("Record button pressed")
+        RecordingLabel.text = "Recording in prgress"
+        stopRecordingButton.enabled = true
+        recordButton.enabled = false
+    }
+    
+    @IBAction func stopRecording(sender: AnyObject) {
+        recordButton.enabled = true
+        stopRecordingButton.enabled = false
+        RecordingLabel.text = "Tap to Record"
+    }
+    override func viewWillAppear(animated: Bool) {
+        stopRecordingButton.enabled = false
+    }
 }
 
---
+++ UdacityNanoDegree/MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//: This is a variable of type float
+var mySalary: Float
+
+
+struct student {
+    let name: String
+    var age: Int
+    var school: String
+}
+
+var ayush = student(name: "Ayush", age: 18, school: "Udacity")
