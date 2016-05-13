51f9b185f28fbcbec93ace6e6246e04340e8e177
Yang Zhou <zhouyang6828629@gmail.com>
2016-05-13 09:57:07 -0700

    Add function mines

--- UdacityNanoDegree/beginning-ios-pirate-fleet-pirate-fleet-1-init/Pirate Fleet/ControlCenter.swift
+++ UdacityNanoDegree/beginning-ios-pirate-fleet-pirate-fleet-1-init/Pirate Fleet/ControlCenter.swift
@@ -17,18 +17,42 @@ struct Ship {
     let isVertical: Bool
 }
 
+struct Mine: _Mine_ {
+    let location: GridLocation
+    let explosionText: String
+}
+
 class ControlCenter {
     
     func addShipsAndMines(human: Human) {
-        <#WRITE YOUR CODE HERE!#>
+        let mediumShip1 = Ship(length: 3, location: GridLocation(x:0, y:0), isVertical: false)
+        human.addShipToGrid(mediumShip1)
+        
+        let mediumShip2 = Ship(length: 3, location: GridLocation(x:0, y:1), isVertical: false)
+        human.addShipToGrid(mediumShip2)
+        
+        let smallShip = Ship(length: 2, location: GridLocation(x:0, y:2), isVertical: false)
+        human.addShipToGrid(smallShip)
+        
+        let largeShip = Ship(length: 4, location: GridLocation(x:0, y:4), isVertical: false)
+        human.addShipToGrid(largeShip)
+        
+        let extraLargeShip = Ship(length: 5, location: GridLocation(x:0, y:5), isVertical: false)
+        human.addShipToGrid(extraLargeShip)
+        
+        let mine1 = Mine(location: GridLocation(x:5, y:5), explosionText: "HaHa")
+        human.addMineToGrid(mine1)
+        
+        let mine2 = Mine(location: GridLocation(x:6, y:6), explosionText: "HaHa")
+        human.addMineToGrid(mine2)
     }
     
     func calculateFinalScore(gameStats: GameStats) -> Int {
         
         var finalScore: Int
         
-        finalScore = 0                
-        
+        finalScore = (5 - gameStats.enemyShipsRemaining) * gameStats.sinkBonus + (5 - gameStats.humanShipsSunk) * gameStats.shipBonus - (gameStats.numberOfHitsOnEnemy + gameStats.numberOfMissesByHuman) * gameStats.guessPenalty
+        print("the value of final score is \(finalScore)")
         return finalScore
     }
 }
